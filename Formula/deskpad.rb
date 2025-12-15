08026b56845781e93077af16a22874abe1805166
Luis Suárez <isamudev@luissuarez.dev>
2025-12-14 22:11:47 -0300

    Formula DeskPad

---
+++ Formula/deskpad.rb
@@ -0,0 +1,18 @@
+cask "deskpad" do
+  version "1.4.0"
+  sha256 "30ec34736143921d016d1d1a1c68a3e5f3598abb29877bd66bfa905bf11ef4e0"
+
+  url "https://github.com/luewell/DeskPad/releases/download/v#{version}/DeskPad.app.zip"
+  name "DeskPad"
+  desc "Virtual monitor for screen sharing"
+  homepage "https://github.com/luewell/DeskPad"
+
+  depends_on macos: ">= :sonoma"
+
+  app "DeskPad.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.luewell.DeskPad",
+    "~/Library/Containers/com.luewell.DeskPad",
+  ]
+end
